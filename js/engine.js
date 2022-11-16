eb58401cfd7b1438ed77a71229f11a6efc02c0b4
Aleksey Zgeria <63537531+yukehi@users.noreply.github.com>
2022-11-16 22:04:13 +0200

    language changed
    for prisntion language changed

--- js/engine.js
+++ js/engine.js
@@ -27,210 +27,211 @@ $('.btn').click(function(){
 function createResult(emotion, color) {
   if(emotion === 'Joy'){
     if(color === 'yellow'){
-      alert('you feel now Super Happy')
+      alert('את|ה מרגיש|ה מאושר|ת ')
     }
     if(color === 'lime'){
-      alert('you feel now in love')
+      alert('את|ה מרגיש|ה מאוהב|ת')
     }
     if(color === 'green'){
-      alert('you feel now guilt')
+      alert('את|ה מרגיש|ה אשמ|ה')
     }
     if(color === 'aqua'){
-      alert('you feel now delight')
+      alert('את|ה מרגיש|ה משועשע|ת')
     }
     if(color === 'blue'){
-      alert('you feel now Confus')
+      alert('את|ה מרגיש|ה מבולבל|ת')
     }
     if(color === 'pink'){
-      alert('you feel now sick')
+      alert('את|ה מרגיש|ה חולה')
     }
     if(color === 'red'){
-      alert('you feel now pride')
+      alert('את|ה מרגיש|ה גאווה')
     }
     if(color === 'orange'){
-      alert('you feel now optimist')
+      alert('את|ה מרגיש|ה אופטימי')
     }
   }
   if(emotion === 'Trust'){
     if(color === 'yellow'){
-      alert('you feel now in love')
+      alert('את|ה מרגיש|ה מאוהב|ת')
     }
     if(color === 'lime'){
-      alert('you feel now super trusted')
+      alert('את|ה מרגיש|ה מאובטח|ת')
     }
     if(color === 'green'){
-      alert('you feel now subIVIission')
+      alert('את|ה מרגיש|ה חלוק|ה בדעתך')
     }
     if(color === 'aqua'){
-      alert('you feel now curiosity')
+      alert('את|ה מרגיש|ה מסוקרנ|ת')
     }
     if(color === 'blue'){
-      alert('you feel now sentimentality')
+      alert('את|ה מרגיש|ה סנטימנטלי|ת')
     }
     if(color === 'pink'){
-      alert('you feel now confus')
+      alert('את|ה מרגיש|ה מבולבל|ת')
     }
     if(color === 'red'){
-      alert('you feel now dominace')
+      alert('את|ה מרגיש|ה בשליטה')
     }
     if(color === 'orange'){
-      alert('you feel now hopeful')
+      alert('את|ה מרגיש|ה תקווה')
     }
   }
   if(emotion === 'Feared'){
     if(color === 'yellow'){
-      alert('you feel now guilted')
+      alert('את|ה מרגיש|ה אשמ|ה')
     }
     if(color === 'lime'){
-      alert('you feel now subIVIission')
+      alert('את|ה מרגיש|ה חלוק|ה בדעתך')
     }
     if(color === 'green'){
-      alert('you feel now frightened')
+      alert('את|ה מרגיש|ה מובהל|ת')
     }
     if(color === 'aqua'){
-      alert('you feel now awesome')
+      alert('את|ה מרגיש|ה מדהימ|ה')
     }
     if(color === 'blue'){
-      alert('you feel now despair')
+      alert('את|ה מרגיש|ה מיואש|ת')
     }
     if(color === 'pink'){
-      alert('you feel now outrage')
+      alert('את|ה מרגיש|ה ממורמר|ת')
     }
     if(color === 'red'){
-      alert('you feel now stormy')
+      alert('את|ה מרגיש|הס סוער|')
     }
     if(color === 'orange'){
-      alert('you feel now anxitey')
+      alert('את|ה מרגיש|ה חרדתי|ת')
     }
   }
   if(emotion === 'Suprised'){
     if(color === 'yellow'){
-      alert('you feel now delight')
+      alert('את|ה מרגיש|ה משועשע|ת')
     }
     if(color === 'lime'){
-      alert('you feel now curiosity')
+      alert('את|ה מרגיש|ה סקרנ|ית')
     }
     if(color === 'green'){
-      alert('you feel now awe')
+      alert('את|ה מרגיש|ה מכובד|ת')
     }
     if(color === 'aqua'){
-      alert('you feel now suprised')
+      alert('את|ה מרגיש|ה מופתע|ת')
     }
     if(color === 'blue'){
-      alert('you feel now disapproval')
+      alert('את|ה מרגיש|ה גינוי|ה')
     }
     if(color === 'pink'){
-      alert('you feel now unbelief')
+      alert('את|ה מרגיש|ה אי אמון')
     }
     if(color === 'red'){
-      alert('you feel now outrage')
+      alert('את|ה מרגיש|ה ממורמר|ת')
     }
     if(color === 'orange'){
-      alert('you feel now confus')
+      alert('את|ה מרגיש|ה מבולבל|ת')
     }
   }
   if(emotion === 'Sad'){
     if(color === 'yellow'){
-      alert('you feel now confus')
+      alert('את|ה מרגיש|ת מבולבל|ת')
     }
     if(color === 'lime'){
-      alert('you feel now sentimentality')
+      alert('את|ה מרגיש|ת סינטמנטלי|ת')
     }
     if(color === 'green'){
-      alert('you feel now despair')
+      alert('את|ה מרגיש|ת מיואש|ת')
     }
     if(color === 'aqua'){
-      alert('you feel now disaproval')
+      alert('את|ה מרגיש|ת מורד|ת')
     }
     if(color === 'blue'){
-      alert('you feel now depressed ')
+      alert('את|ה מרגיש|ת מדוכא|ה ')
     }
     if(color === 'pink'){
-      alert('you feel now remorse')
+      alert('את|ה מרגיש|ת חרטה')
     }
     if(color === 'red'){
-      alert('you feel now envy')
+      alert('את|ה מרגיש|ת קינאה')
     }
     if(color === 'orange'){
-      alert('you feel now pessimism')
+      alert('את|ה מרגיש|ת פסימי|ת')
     }
   }
   if(emotion === 'Disgust'){
     if(color === 'yellow'){
-      alert('you feel now morbidness')
+      alert('את|ה מרגיש|ה הרסני|ת')
     }
     if(color === 'lime'){
-      alert('you feel now confus')
+      alert('את|ה מרגיש|ה מבולבל|ת')
     }
     if(color === 'green'){
-      alert('you feel now shame')
+      alert('את|ה מרגיש|ה בושה')
     }
     if(color === 'aqua'){
-      alert('you feel now unbelief')
+      alert('את|ה מרגיש|ה אי אמון')
     }
     if(color === 'blue'){
-      alert('you feel now remorse ')
+      alert('את|ה מרגיש|ה חרטה ')
     }
     if(color === 'pink'){
-      alert('you feel now super Disgust')
+      alert('את|ה מרגיש|ה נגעל|ת')
     }
     if(color === 'red'){
-      alert('you feel now contempted')
+      alert('את|ה מרגיש|ה זלזול')
     }
     if(color === 'orange'){
-      alert('you feel now cynicim')
+      alert('את|ה מרגיש|ה לגלוג')
     }
   }
   if(emotion === 'Angry'){
     if(color === 'yellow'){
-      alert('you feel now pride')
+      alert('את|ה מרגיש|ה גאווה')
     }
     if(color === 'lime'){
-      alert('you feel now dominance')
+      alert('את|ה מרגיש|ה דומיננטי|ת')
     }
     if(color === 'green'){
-      alert('you feel now confus')
+      alert('את|ה מרגיש|ה בלבול')
     }
     if(color === 'aqua'){
-      alert('you feel now outrage')
+      alert('את|ה מרגיש|ה זוועה')
     }
     if(color === 'blue'){
-      alert('you feel now envy ')
+      alert('את|ה מרגיש|ה קנאי|ת ')
     }
     if(color === 'pink'){
-      alert('you feel now super contempted')
+      alert('את|ה מרגיש|ה זלזול')
     }
     if(color === 'red'){
-      alert('you feel now rage')
+      alert('את|ה מרגיש|ה זעם')
     }
     if(color === 'orange'){
-      alert('you feel now aggresivness')
+      alert('את|ה מרגיש|ה אגרסיביות')
     }
   }
   if(emotion === 'anticipated'){
     if(color === 'yellow'){
-      alert('you feel now optimism')
+      alert('את|ה מרגיש|ה אופטימי|ת')
     }
     if(color === 'lime'){
-      alert('you feel now hopeful')
+      alert('את|ה מרגיש|ה תקווה')
     }
     if(color === 'green'){
-      alert('you feel now anxiety')
+      alert('את|ה מרגיש|ה חרדה')
     }
     if(color === 'aqua'){
-      alert('you feel now confus')
+      alert('את|ה מרגיש|ה מבולבל')
     }
     if(color === 'blue'){
-      alert('you feel now pessimism ')
+      alert('את|ה מרגיש|ה פסימי|ת ')
     }
     if(color === 'pink'){
-      alert('you feel now cynicism')
+      alert('את|ה מרגיש|ה ציני|ת')
     }
     if(color === 'red'){
-      alert('you feel now aggresivness')
+      alert('את|ה מרגיש|ה אגרסיבי|ת')
     }
     if(color === 'orange'){
-      alert('you feel now Anticipated')
+      alert('את|ה מרגיש|ה מצפה')
     }
   }
+   
 }
